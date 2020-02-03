05ba104263b70fbc9e4738d72550f7500c5ad6e9
Kyle Knaggs <kyle@adfoam.com>
2020-02-03 10:13:56 -0500

    Add $$phase field to $apply and $digest.
    Contains the 1st test for the "Scope Phases" section in Chapter 2.

--- src/scope.js
+++ src/scope.js
@@ -8,6 +8,7 @@ function Scope() {
 	this.$$watchers = [];
   this.$$lastDirtyWatch = null;
   this.$$asyncQueue = [];
+  this.$$phase = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
@@ -33,11 +34,22 @@ Scope.prototype.$evalAsync = function(expr) {
   this.$$asyncQueue.push({scope: this, expression: expr});
 };
 
+Scope.prototype.$beginPhase = function (phase){
+  if (this.$$phase){
+    throw this.$$phase + ' already in progress.';
+  }
+  this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+  this.$$phase = null;
+};
 
 Scope.prototype.$digest = function() {
   var ttl = 10;
   var dirty;
   this.$$lastDirtyWatch = null;
+  this.$beginPhase('$digest');
   do {
     while (this.$$asyncQueue.length) {
       var asyncTask = this.$$asyncQueue.shift();
@@ -45,9 +57,11 @@ Scope.prototype.$digest = function() {
     }
     dirty = this.$$digestOnce();
     if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
+      this.$clearPhase();
       throw '10 digest iterations reached';
     }
   } while (dirty || this.$$asyncQueue.length);
+  this.$clearPhase();
 };
 
 Scope.prototype.$$digestOnce = function() {
@@ -90,8 +104,10 @@ Scope.prototype.$eval = function(expr, locals){
 
 Scope.prototype.$apply = function(expr) {
   try {
+    this.$beginPhase('$apply');
     return this.$eval(expr);
   } finally {
+    this.$clearPhase();
     this.$digest();
   }
 };
