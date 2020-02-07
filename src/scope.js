7f3197d9f124f890bca7db56d373dfd6c2ef139a
Kyle Knaggs <kyle@adfoam.com>
2020-02-06 19:17:31 -0500

    Add postDigest
    Contains the 1st and 2nd tests for the "Running Code After A
    Digest - $$postDigest" section in Chapter 2.

--- src/scope.js
+++ src/scope.js
@@ -10,6 +10,7 @@ function Scope() {
   this.$$asyncQueue = [];
   this.$$applyAsyncQueue = [];
   this.$$applyAsyncId = null;
+  this.$$postDigestQueue = [];
   this.$$phase = null;
 }
 
@@ -78,6 +79,10 @@ Scope.prototype.$digest = function() {
     }
   } while (dirty || this.$$asyncQueue.length);
   this.$clearPhase();
+
+  while (this.$$postDigestQueue.length) {
+    this.$$postDigestQueue.shift()();
+  }
 };
 
 Scope.prototype.$$digestOnce = function() {
@@ -145,6 +150,10 @@ Scope.prototype.$$flushApplyAsync = function() {
     this.$$applyAsyncQueue.shift()();
   }
   this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$$postDigest = function(fn) {
+  this.$$postDigestQueue.push(fn);
 };
 
 module.exports = Scope;
