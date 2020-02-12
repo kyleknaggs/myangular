c4e60c0113c19d9196a644915bd5af84b9d5b492
Kyle Knaggs <kyle@adfoam.com>
2020-02-12 10:37:23 -0500

    Add $watchGroup
    Contains the 1st and 2nd tests in the "Watching Several Changes
    With One Listener: $watch-Group" section in Chapter 2.

--- src/scope.js
+++ src/scope.js
@@ -168,4 +168,27 @@ Scope.prototype.$$postDigest = function(fn) {
   this.$$postDigestQueue.push(fn);
 };
 
+Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
+  var self = this;
+  var oldValues = new Array(watchFns.length);
+  var newValues = new Array(watchFns.length);
+  var changeReactionScheduled = false;
+
+  function watchGroupListener() {
+    listenerFn(newValues, oldValues, self);
+    changeReactionScheduled = false;
+  }
+
+  _.forEach(watchFns, function(watchFn, i) {
+    self.$watch(watchFn, function(newValue, oldValue) {
+      newValues[i] = newValue;
+      oldValues[i] = oldValue;
+      if (!changeReactionScheduled) {
+        changeReactionScheduled = true;
+        self.$evalAsync(watchGroupListener);
+      }
+    });
+  });
+};
+
 module.exports = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -775,6 +775,51 @@ describe('Scope', function() {
       expect(didRun).toBe(true);
     });
 
-});
+  });
+
+  describe('$watchGroup', function() {
+
+    var scope;
+    beforeEach(function() {
+      scope = new Scope();
+    });
+
+    it('takes watches as an array and calls listener with arrays', function() {
+      var gotNewValues, gotOldValues;
+
+      scope.aValue = 1;
+      scope.anotherValue = 2;
+
+      scope.$watchGroup([
+        function(scope) { return scope.aValue; },
+        function(scope) { return scope.anotherValue; }
+        ], function(newValues, oldValues, scope) {
+          gotNewValues = newValues;
+          gotOldValues = oldValues;
+      });
+      scope.$digest();
+
+      expect(gotNewValues).toEqual([1, 2]);
+      expect(gotOldValues).toEqual([1, 2]);
+    });
+
+    it('only calls listener once per digest', function() {
+      var counter = 0;
+
+      scope.aValue = 1;
+      scope.anotherValue = 2;
+
+      scope.$watchGroup([
+        function(scope) { return scope.aValue; },
+        function(scope) { return scope.anotherValue; }
+      ], function(newValues, oldValues, scope) {
+        counter++;
+      });
+      scope.$digest();
+
+      expect(counter).toEqual(1);
+    });
+
+  });
 
 });
