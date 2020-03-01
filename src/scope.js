17afde245b4535f1a74f142869acd15f12812880
Kyle Knaggs <kyle@adfoam.com>
2020-03-01 14:38:05 -0500

    Execute $applyAsync functions on isolated scopes
    Contains the 8th test in the "Isolated Scopes" section of Chapter
    3.

--- src/scope.js
+++ src/scope.js
@@ -101,8 +101,8 @@ Scope.prototype.$digest = function() {
   this.$root.$$lastDirtyWatch = null;
   this.$beginPhase('$digest');
 
-  if (this.$$applyAsyncId) {
-    clearTimeout(this.$$applyAsyncId);
+  if (this.$root.$$applyAsyncId) {
+    clearTimeout(this.$root.$$applyAsyncId);
     this.$$flushApplyAsync();
   }
 
@@ -214,8 +214,8 @@ Scope.prototype.$applyAsync = function(expr) {
   self.$$applyAsyncQueue.push(function() {
     self.$eval(expr);
   });
-  if (self.$$applyAsyncId === null) {
-    self.$$applyAsyncId = setTimeout(function() {
+  if (self.$root.$$applyAsyncId === null) {
+    self.$root.$$applyAsyncId = setTimeout(function() {
       self.$apply(_.bind(self.$$flushApplyAsync, self));
     }, 0);
   }
@@ -229,7 +229,7 @@ Scope.prototype.$$flushApplyAsync = function() {
       console.error(e);
     }
   }
-  this.$$applyAsyncId = null;
+  this.$root.$$applyAsyncId = null;
 };
 
 Scope.prototype.$$postDigest = function(fn) {
@@ -243,6 +243,7 @@ Scope.prototype.$new = function(isolated) {
     child.$root = this.$root;
     child.$$asyncQueue = this.$$asyncQueue;
     child.$$postDigestQueue = this.$$postDigestQueue;
+    child.$$applyAsyncQueue = this.$$applyAsyncQueue;
   } else {
     var ChildScope = function() { };
     ChildScope.prototype = this;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1210,6 +1210,19 @@ describe('Scope', function() {
       expect(child.didPostDigest).toBe(true);
     });
 
+    it("executes $applyAsync functions on isolated scopes", function() {
+      var parent = new Scope();
+      var child = parent.$new(true);
+      var applied = false;
+    
+      parent.$applyAsync(function() {
+        applied = true;
+      });
+      child.$digest();
+    
+      expect(applied).toBe(true);
+    });
+
   });
 
 });
