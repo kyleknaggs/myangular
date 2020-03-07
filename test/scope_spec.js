5dc3e6edf7366f9d4747fecadefb43a196d71021
Kyle Knaggs <kyle@adfoam.com>
2020-03-07 10:51:30 -0500

    Notice attribute additions and modifications
    Contains the 1st and 2nd tests in the "Detecting New or Replaced
    Attributes in Objects" sections of Chapter 4.

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1511,7 +1511,52 @@ describe('Scope', function() {
       scope.obj = {a: 1};
       scope.$digest();
       expect(scope.counter).toBe(2);
-      
+
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+
+
+    it('notices when an attribute is added to an object', function() {
+      scope.counter = 0;
+      scope.obj = {a: 1};
+
+      scope.$watchCollection(
+        function(scope) { return scope.obj; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.obj.b = 2;
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+
+    it('notices when an attribute is changed in an object', function() {
+      scope.counter = 0;
+      scope.obj = {a: 1};
+
+      scope.$watchCollection(
+        function(scope) { return scope.obj; },
+        function(newValue, oldValue, scope) {
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+
+      scope.obj.a = 2;
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+
       scope.$digest();
       expect(scope.counter).toBe(2);
     });
